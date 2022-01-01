7222113a70040a273e6c1be0cb9e0b993b5d94b1
ThatBigDerp <darkhacker8604@protonmail.com>
2022-01-01 19:27:45 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// $config = "app.ini";
+// if (file_exists('./config/'.$config)) {
+//     header("Location: ./site/home.php"); 
+// } else {
+//     header("Location: ./admin/login.php"); 
+// }
+// exit();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dulo</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="loader triangle">
+        <svg viewBox="0 0 86 80">
+            <polygon points="43 8 79 72 7 72"></polygon>
+        </svg>
+    </div>
+</body>
+</html>
